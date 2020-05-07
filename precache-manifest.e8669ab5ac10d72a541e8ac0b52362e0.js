12c7747a5e1781e7a8af08a049327f7f0751e1f6
Abhinath Kumar <abhinathkumar@gmail.com>
2020-05-07 15:52:45 +0100

    Updates

---
+++ precache-manifest.e8669ab5ac10d72a541e8ac0b52362e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1429b468a3f45969f31bf1ae3d7ad09e",
+    "url": "/holorepository-website/index.html"
+  },
+  {
+    "revision": "e042f3ac70be9c98ef62",
+    "url": "/holorepository-website/static/css/main.eed2b97c.chunk.css"
+  },
+  {
+    "revision": "290883112a865e798de9",
+    "url": "/holorepository-website/static/js/2.3a8b4da0.chunk.js"
+  },
+  {
+    "revision": "e042f3ac70be9c98ef62",
+    "url": "/holorepository-website/static/js/main.cc69898b.chunk.js"
+  },
+  {
+    "revision": "95534ec7c92b170e9920",
+    "url": "/holorepository-website/static/js/runtime~main.e9780457.js"
+  },
+  {
+    "revision": "451c266ff2130c8a2e14c147f48a9eaf",
+    "url": "/holorepository-website/static/media/background.451c266f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HoloRepository-Website/precache-manifest.582f9ce0165b3127a88e9f55324bc4ba.js"
+  "/holorepository-website/precache-manifest.e8669ab5ac10d72a541e8ac0b52362e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HoloRepository-Website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/holorepository-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
